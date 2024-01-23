817eea2bdc39aec0bf6c1bd5b4a257c2e845227d
Ahmet Şeker <shekerahmet80@gmail.com>
2024-01-23 16:11:44 +0300

    not-found pages updated

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -10,7 +10,7 @@ const NotFound = () => {
                 </div>
                 <div className="col-md-4 d-flex flex-column pt-4 text-center text-md-start">
                     <h2 className="text-primary fs-1">Not Found</h2>
-                    <p>The page you requested was not found</p>
+                    <p>The page you requested was not found...</p>
                 </div>
             </div>
         </div>
